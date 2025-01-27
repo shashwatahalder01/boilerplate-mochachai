0f57e80fcd07b6ae4c91d2c3a3abad83574628de
shashwata <shashwatahalder01@gmail.com>
2025-01-27 13:57:53 +0600

    Fix issues

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -98,7 +98,6 @@ suite("Functional Tests with Zombie.js", function () {
                 browser.assert.text("span#name", "Marco");
                 browser.assert.text("span#surname", "Colombo");
                 browser.assert.elements("span#dates", 1);
-                done(); 
               });
             });
         });
@@ -114,10 +113,9 @@ suite("Functional Tests with Zombie.js", function () {
             .then(() => {
               browser.pressButton("submit", function () {
                 browser.assert.success();
-                browser.assert.text("span#name", "Marco");
-                browser.assert.text("span#surname", "Colombo");
+                browser.assert.text("span#name", "Amerigo");
+                browser.assert.text("span#surname", "Vespucci");
                 browser.assert.elements("span#dates", 1);
-                done(); 
               });
             });
         });
